b1c97647f574bfdaf92b9468b606020c8cb0ffa5
Erik Larkö <erik@larko.se>
2017-09-09 14:39:14 +0200

    Fucking hell with the status bar height should be fucking solved fucking now

--- App.js
+++ App.js
@@ -3,11 +3,26 @@ import { StackNavigator } from 'react-navigation';
 import { answerService } from './src/services/answer-service.js';
 import { HomeScreen } from './src/components/HomeScreen.js';
 import { SessionScreen } from './src/components/SessionScreen.js';
+import { StatusBar, Platform } from 'react-native';
 
 answerService.setAnswerPool(['a', 'b', 'c']);
 
+const statusBarHeight = Platform.OS === 'ios'
+    ? 20
+    : StatusBar.currentHeight;
+
+const headerHeightFix = {
+    navigationOptions: {
+        headerStyle: {
+            marginTop: statusBarHeight,
+        },
+    }
+};
+
+// ALL ROUTES HERE MUST CONTAIN THE ...headerHeightFix SHIT TO
+// MAKE THE VIEW RENDER BELOW THE STATUS BAR
 export default App = StackNavigator({
-    Home: { screen: HomeScreen },
-    Session: { screen: SessionScreen },
+    Home: { screen: HomeScreen, ...headerHeightFix },
+    Session: { screen: SessionScreen, ...headerHeightFix },
 });
 
