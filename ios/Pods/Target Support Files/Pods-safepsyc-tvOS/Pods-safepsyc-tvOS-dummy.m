d4a457d5cfde630fdc7bd937620b3274195643cb
a b <mad@as-MacBook-Pro.local>
2017-10-10 16:49:59 +0200

    Can run react-native run-ios and browse around! Just a few header height issues

---
+++ ios/Pods/Target Support Files/Pods-safepsyc-tvOS/Pods-safepsyc-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_safepsyc_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_safepsyc_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-safepsyc/Pods-safepsyc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_safepsyc : NSObject
+@end
+@implementation PodsDummy_Pods_safepsyc
+@end
