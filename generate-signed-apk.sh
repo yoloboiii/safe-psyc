f13af2ab52e74a24ad1ce99d64546b541492562c
Erik Larkö <erik@larko.se>
2018-03-14 00:04:43 +0100

    Make the release script work with secrets from the SECRETS folder

--- generate-signed-apk.sh
+++ generate-signed-apk.sh
@@ -1,6 +1,49 @@
 set -eu
 
-trap 'cd - > /dev/null' EXIT
+ABS_WD=$(dirname $(readlink -f $0))
+
+
+function placeKeyStore {
+    cp "$ABS_WD/SECRETS/safe-psyc-release-privkey.keystore" "$ABS_WD/android/app"
+}
+
+function removeKeyStore {
+    rm -f "$ABS_WD/android/app/safe-psyc-release-privkey.keystore"
+}
+
+
+function placeGradlePropertiesSecrets {
+    cat "$ABS_WD/SECRETS/release-gradle-properties" >> "$ABS_WD/android/gradle.properties"
+}
+
+function removeGradlePropertiesSecrets {
+    sed -i '/RELEASE_/d' "$ABS_WD/android/gradle.properties"
+}
+
+
+function cleanup {
+    cd - > /dev/null
+
+    removeGradlePropertiesSecrets
+    removeKeyStore
+}
+
+
+trap cleanup EXIT
+
+
+placeGradlePropertiesSecrets
+placeKeyStore
+
 cd android
-./gradlew assembleReleased
-echo "The APK can be found at $(readlink -f ./app/build/outputs/apk/app-release.apk)"
+./gradlew assembleRelease
+
+
+echo
+echo
+echo
+echo "The APK can be found at $(readlink -f ./app/build/outputs/apk/release/app-release.apk)"
+echo
+echo "Please make sure there is no .keystore file in android/app"
+echo "and no RELEASE_ keys in android/gradle.properties"
+
