7b2c02e21e8779e72b436b0d4c49f3b2e4ecf4fd
Erik Larkö <erik@larko.se>
2018-03-18 10:31:22 +0100

    Refactored the reset password screen a bit

--- src/components/ResetPasswordScreen.js
+++ src/components/ResetPasswordScreen.js
@@ -7,6 +7,7 @@ import { FancyInput } from './Inputs.js';
 import { NavigationActions } from 'react-navigation';
 import { LargeButton, StandardButton } from './Buttons.js';
 import { StandardText } from './Texts.js';
+import { ActivityIndicator } from './ActivityIndicator.js';
 import { VerticalSpace } from './VerticalSpace.js';
 import { userBackendFacade } from '../services/user-backend.js';
 import { constants } from '../styles/constants.js';
@@ -19,7 +20,7 @@ type Props = {
 };
 type State = {
     email: string,
-    emailSent: boolean,
+    status: 'not-sent' | 'sending' | 'success' | Error,
 };
 export class ResetPasswordScreen extends React.Component<Props, State> {
     static navigationOptions = {
@@ -32,65 +33,95 @@ export class ResetPasswordScreen extends React.Component<Props, State> {
         const email = props.navigation.state ? props.navigation.state.params.email || '' : '';
         this.state = {
             email: email,
-            emailSent: false,
+            status: 'not-sent',
         };
     }
 
     _sendResetEmail() {
-        userBackendFacade
-            .resetPassword(this.state.email)
-            .then(() => {
-                this.setState({
-                    emailSent: true,
+        this.setState({ status: 'sending' }, () => {
+            userBackendFacade
+                .resetPassword(this.state.email)
+                .then(() => {
+                    this.setState({
+                        status: 'success',
+                    });
+                })
+                .catch(e => {
+                    this.setState({
+                        status: e,
+                    }, () => {
+                        Alert.alert('Unable to send reset email', e.message);
+                    });
                 });
+        });
+    }
+
+    _done() {
+        this.props.navigation.dispatch(
+            NavigationActions.reset({
+                index: 0,
+                actions: [
+                    NavigationActions.navigate({
+                        routeName: 'Login',
+                    }),
+                ],
             })
-            .catch(e => {
-                Alert.alert('Unable to send reset email', e.message);
-            });
+        );
     }
 
     render() {
-        if (this.state.emailSent) {
-            return (
-                <View style={constants.flex1}>
-                    <StandardText>Password reset email sent!</StandardText>
-                    <VerticalSpace />
-                    <StandardButton
-                        title={'Back to login'}
-                        onPress={() =>
-                            this.props.navigation.dispatch(
-                                NavigationActions.reset({
-                                    index: 0,
-                                    actions: [
-                                        NavigationActions.navigate({
-                                            routeName: 'Login',
-                                        }),
-                                    ],
-                                })
-                            )
-                        }
-                    />
-                </View>
-            );
-        }
+        return <SpaceBetween>
+                { this._renderTop() }
+                { this._renderBottom() }
+        </SpaceBetween>
+    }
 
-        return (
-            <View style={{
+    _renderTop() {
+        if (this.state.status === 'success'){
+            return <StandardText>Password reset email sent!</StandardText>
+        } else {
+
+            return <FancyInput
+                label={'EMAIL'}
+                value={this.state.email}
+                onChange={text => this.setState({ email: text })}
+                disabled={this.state.status === 'sending'}
+            />
+        }
+    }
+
+    _renderBottom() {
+        const { status } = this.state;
+
+        if (status === 'not-sent') {
+            return <LargeButton
+                title="Send reset email"
+                onPress={this._sendResetEmail.bind(this)}
+            />
+        } else if (status === 'sending') {
+            return <ActivityIndicator color={constants.primaryColor} size="large" />
+        } else if (status === 'success') {
+            return <LargeButton
+                title="Back to login"
+                onPress={this._done.bind(this)}
+            />
+        } else if (status instanceof Error) {
+            return <LargeButton
+                title="Send reset email"
+                onPress={this._sendResetEmail.bind(this)}
+            />
+        } else {
+            return <StandardText>Unknown status {status} </StandardText>
+        }
+    }
+}
+
+function SpaceBetween(props) {
+    return <View style={{
                 flex: 1,
                 flexDirection: 'column',
                 justifyContent: 'space-between',
 
                 margin: constants.space(),
-            }}>
-                <FancyInput
-                    label={'EMAIL'}
-                    value={this.state.email}
-                    onChange={text => this.setState({ email: text })}
-                />
-
-                <VerticalSpace />
-                <LargeButton onPress={this._sendResetEmail.bind(this)} title={'Send reset email'} />
-            </View>
-        );
-    }
+    }} { ...props } />
 }
